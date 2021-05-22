36e90889fc63577e063379cfc4ad91ee599d41e9
jackjnn <jacknyangenj.jn2@gmail.com>
2021-05-22 12:24:34 +0300

    Implemented fetching national data

--- app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.covidtracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.security.auth.callback.Callback
 
 private const val BASE_URL = "https://covidtracking.com/api/v1/"
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var nationalDailyData: List<CovidData>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,7 +27,28 @@ class MainActivity : AppCompatActivity() {
             .build()
         val covidService = retrofit.create(CovidService::class.java)
         //fetch the national data
-        covidService.getNationalData()
+            covidService.getNationalData().enqueue(object: Callback<List<CovidData>>,
+                retrofit2.Callback<List<CovidData>> {
+                override fun onResponse(
+                    call: Call<List<CovidData>>,
+                    response: Response<List<CovidData>>
+                ) {
+                    Log.i(TAG, "onResponse $response")
+                    val nationalData = response.body()
+                    if (nationalData == null) {
+                        Log.w(TAG, "Did not receive a valid response body")
+                        return
+                    }
+                    nationalDailyData = nationalData.reversed()
+                    Log.i(TAG, "Update graph with national data")
+                    // TODO: Update graph with national data
+                }
+
+                override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
+                    Log.e(TAG, "onFailure $t")
+                }
+
+            })
 
         //fetch the state data
 
